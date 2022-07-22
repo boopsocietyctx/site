16e4de355107b2ef9e2f2bd9fc61cb75fc96a85c
Richard Simpson <richardsimpson@outlook.com>
2022-07-22 00:39:02 -0500

    the rest of the owl

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import { Box, Container, Flex, Heading, VStack, Text, Link } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading, VStack, Text, Link, Stack, Center } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/future/image';
 import boopHero from '../public/boop-hero.png';
-import instagramEmbed from './ig_embed.html';
 import twitterEmbed from './twitter_embed.html';
 import NextLink from 'next/link';
+
+import clutchPic from '../public/board/clutch.jpg';
+import polygonPic from '../public/board/polygon.jpg';
+import sharpPic from '../public/board/sharp.jpg';
 
 const Home: NextPage = () => {
     return (
@@ -17,22 +20,20 @@ const Home: NextPage = () => {
             </Head>
 
             <Container as="main" maxWidth="120ch">
-                <Box position="sticky" top={10} right={0}>
-                    <VStack alignItems="end">
-                        <NextLink href="/" passHref>
-                            <Link textStyle="navMenuLink">Home</Link>
-                        </NextLink>
-                        <NextLink href="/" passHref>
-                            <Link textStyle="navMenuLink">Events</Link>
-                        </NextLink>
-                        <NextLink href="/" passHref>
-                            <Link textStyle="navMenuLink">Board</Link>
-                        </NextLink>
-                    </VStack>
-                </Box>
+                <VStack bg="rgba(23, 25, 35, 0.8)" p="4" borderRadius="xl" float="right" position="sticky" top={10} right={0} alignItems="end">
+                    <NextLink href="/" passHref>
+                        <Link textStyle="navMenuLink">Home</Link>
+                    </NextLink>
+                    <NextLink href="/" passHref>
+                        <Link textStyle="navMenuLink">Events</Link>
+                    </NextLink>
+                    <NextLink href="/" passHref>
+                        <Link textStyle="navMenuLink">Board</Link>
+                    </NextLink>
+                </VStack>
                 <Flex
                     flexDirection={{ md: 'row', base: 'column' }}
-                    height="90vh"
+                    height="99vh"
                     alignItems="center"
                     justifyContent={{ md: 'flex-end', base: 'center' }}
                     gap="5vw"
@@ -47,15 +48,71 @@ const Home: NextPage = () => {
                         </Heading>
                     </Box>
                 </Flex>
-                <Heading textAlign="center" textStyle="shoutHeading" fontSize="7xl">
-                  Our Recurring Events
-                </Heading>
-                <VStack>
-                  <Box layerStyle="marqueEvent">
-
-                  </Box>
-                </VStack>
-                <div dangerouslySetInnerHTML={{ __html: twitterEmbed }}></div>
+                <Container maxW="90ch">
+                    <Heading id="recurring-events" mb="40" textAlign="center" textStyle="shoutHeading" fontSize="7xl">
+                        Our Recurring Events
+                    </Heading>
+                    <VStack gap="10" mb="3rem" px="1rem">
+                        <Box layerStyle="marqueEvent" alignSelf="flex-start">
+                            <Text textStyle="marqueHeader">Off The Leash - Park Munch/Social</Text>
+                            <Text textStyle="marqueDesc">MUELLER PARK - 2ND SATURDAYS</Text>
+                        </Box>
+                        <Box layerStyle="marqueEvent" alignSelf="center">
+                            <Text textStyle="marqueHeader">Paws On 4th - Bar Night</Text>
+                            <Text textStyle="marqueDesc">OILCAN HARRY'S - 4TH SATURDAYS</Text>
+                        </Box>
+                        <Box layerStyle="marqueEvent" alignSelf="flex-end">
+                            <Text textStyle="marqueHeader">Educational Workshops/Moshes</Text>
+                            <Text textStyle="marqueDesc">EVERY QUARTER</Text>
+                        </Box>
+                    </VStack>
+                    <Heading my="40" textAlign="center" textStyle="shoutHeading" fontSize="7xl">
+                        Board Hoomans
+                    </Heading>
+                    <Stack 
+                      direction={['column', 'column', 'row']} 
+                      alignItems="center" 
+                      spacing="6"
+                      my="20"
+                      >
+                        <Box layerStyle="boardPicContainer">
+                            <Box
+                                as={Image}
+                                src={clutchPic}
+                                alt="Board member pup clutch"
+                                borderColor="yellow.400"
+                                layerStyle="boardPic"
+                            />
+                            <Text>Clutch</Text>
+                        </Box>
+                        <Box layerStyle="boardPicContainer">
+                            <Box
+                                as={Image}
+                                src={polygonPic}
+                                alt="Board member pup polygon"
+                                borderColor="red.500"
+                                layerStyle="boardPic"
+                            />
+                            <Text>Polygon</Text>
+                        </Box>
+                        <Box layerStyle="boardPicContainer">
+                            <Box
+                                as={Image}
+                                src={sharpPic}
+                                alt="Board member pup sharp"
+                                borderColor="white"
+                                layerStyle="boardPic"
+                            />
+                            <Text>Sharp</Text>
+                        </Box>
+                    </Stack>
+                    <Heading mt="40" mb="10" textAlign="center" textStyle="shoutHeading" fontSize="7xl">
+                        Social Updates
+                    </Heading>
+                    <Center>
+                      <div dangerouslySetInnerHTML={{ __html: twitterEmbed }}></div>
+                    </Center>
+                </Container>
             </Container>
 
             <footer></footer>
--- pages/theme.ts
+++ pages/theme.ts
@@ -1,29 +1,67 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, theme as baseTheme, withDefaultColorScheme } from '@chakra-ui/react';
 
-const theme = extendTheme({
-    fonts: {
-        heading: `'Teko', Georgia, serif`,
-    },
-    layerStyle: {
-      marqueEvent: {
-        
-      }
-    },
-    textStyles: {
-        navMenuLink: {
-            textTransform: 'uppercase',
-            fontFamily: 'heading',
-            fontSize: '2xl'
+const theme = extendTheme(
+    {
+        styles: {
+            global: {
+                body: {
+                    bg: 'gray.900',
+                },
+            },
         },
-        shoutHeading: {
-          textTransform: 'uppercase',
-          color: 'red.500',
-          textShadow: '3px 3px black, 4px 4px rgba(255, 255, 255, 0.7)'
-        }
+        colors: {
+          brand: '#004aad',
+        },
+        fonts: {
+            heading: `'Teko', Georgia, serif`,
+        },
+        layerStyles: {
+            marqueEvent: {
+              background: 'gray.300',
+              padding: '1rem',
+              color: 'black',
+              boxShadow: '20px 20px var(--chakra-colors-brand)'
+            },
+            boardPicContainer: {
+              flex: ['1 1 0'],
+              maxWidth: ['80vw', '80vw', 'unset'],
+              textAlign: 'center',
+              fontSize: '4xl',
+              fontFamily: 'heading',
+            },
+            boardPic: {
+              borderRadius: '100%',
+              borderWidth: '8px',
+              borderStyle: 'solid',
+            }
+        },
+        textStyles: {
+            navMenuLink: {
+                textTransform: 'uppercase',
+                fontFamily: 'heading',
+                fontSize: '2xl',
+            },
+            shoutHeading: {
+                textTransform: 'uppercase',
+                color: 'red.500',
+                textShadow: '3px 3px black, 4px 4px rgba(255, 255, 255, 0.5)'
+            },
+            marqueHeader: {
+              fontSize: '2xl',
+              fontWeight: 700,
+              mb: '1',
+            },
+            marqueDesc: {
+              fontSize: 'xl',
+              fontWeight: 400,
+              textTranform: 'uppercase'
+            }
+        },
+        config: {
+            initialColorMode: 'dark',
+        },
     },
-    config: {
-        initialColorMode: 'dark',
-    },
-});
+    withDefaultColorScheme({ colorScheme: 'red' })
+);
 
 export { theme };
