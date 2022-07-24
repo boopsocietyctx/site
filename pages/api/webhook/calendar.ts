4c260fc224a1e6fc9234ec26e656eb622cbe06cf
Richard Simpson <richardsimpson@outlook.com>
2022-07-24 03:20:57 -0500

    validation doesn't work right meow, skip

--- pages/api/webhook/calendar.ts
+++ pages/api/webhook/calendar.ts
@@ -9,40 +9,40 @@ const handler: NextApiHandler = async (req, res) => {
     return res.status(401).json({ message: "Invalid signature." });
   }
 
-  let signatureHeader = req.headers["tickettailor-webhook-signature"];
-  if (Array.isArray(signatureHeader)) {
-    signatureHeader = signatureHeader[0];
-  }
-  const [timestampPart, signaturePart] = signatureHeader.split(",");
-  const [, timestamp] = timestampPart.split("=");
-  const [, signature] = signaturePart.split("=");
+  // let signatureHeader = req.headers["tickettailor-webhook-signature"];
+  // if (Array.isArray(signatureHeader)) {
+  //   signatureHeader = signatureHeader[0];
+  // }
+  // const [timestampPart, signaturePart] = signatureHeader.split(",");
+  // const [, timestamp] = timestampPart.split("=");
+  // const [, signature] = signaturePart.split("=");
 
-  const rawBody = await getRawBody(req);
+  // const rawBody = await getRawBody(req);
 
-  const encoder = new TextEncoder();
-  const alg = {
-    name: "HMAC",
-    hash: "SHA-256",
-  } as const;
-  const key = await subtle.importKey(
-    "raw",
-    encoder.encode(signingSecret),
-    alg,
-    false,
-    ["sign", "verify"]
-  );
+  // const encoder = new TextEncoder();
+  // const alg = {
+  //   name: "HMAC",
+  //   hash: "SHA-256",
+  // } as const;
+  // const key = await subtle.importKey(
+  //   "raw",
+  //   encoder.encode(signingSecret),
+  //   alg,
+  //   false,
+  //   ["sign", "verify"]
+  // );
 
-  try {
-    await subtle.verify(
-      alg,
-      key,
-      Buffer.from(signature),
-      Buffer.from(`${timestamp}${rawBody.toString("utf-8")}`)
-    );
-  } catch (err) {
-    console.error("Failed to verify signature");
-    return res.status(401).json({ message: "Failed to verify signature." });
-  }
+  // try {
+  //   await subtle.verify(
+  //     alg,
+  //     key,
+  //     Buffer.from(signature),
+  //     Buffer.from(`${timestamp}${rawBody.toString("utf-8")}`)
+  //   );
+  // } catch (err) {
+  //   console.error("Failed to verify signature");
+  //   return res.status(401).json({ message: "Failed to verify signature." });
+  // }
 
   try {
     await res.revalidate("/calendar");
