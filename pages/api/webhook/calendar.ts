e67d08a2e8c4efbfab2d82476fccccdb6325663e
Richard Simpson <richardsimpson@outlook.com>
2022-07-26 00:32:41 -0500

    fix: isolate parsing logic too for now

--- pages/api/webhook/calendar.ts
+++ pages/api/webhook/calendar.ts
@@ -14,39 +14,45 @@ const handler: NextApiHandler = async (req, res) => {
   }
 
   let signatureHeader = req.headers["tickettailor-webhook-signature"];
-  if (Array.isArray(signatureHeader)) {
-    signatureHeader = signatureHeader[0];
-  }
-  const [timestampPart, signaturePart] = signatureHeader.split(",");
-  const [, timestamp] = timestampPart.split("=");
-  const [, signature] = signaturePart.split("=");
-
-  const rawBody = await getRawBody(req);
-
-  const encoder = new TextEncoder();
-  const alg = {
-    name: "HMAC",
-    hash: "SHA-256",
-  } as const;
-  const key = await crypto.subtle.importKey(
-    "raw",
-    encoder.encode(signingSecret),
-    alg,
-    false,
-    ["sign", "verify"]
-  );
-
   try {
-    await crypto.subtle.verify(
+    if (Array.isArray(signatureHeader)) {
+      signatureHeader = signatureHeader[0];
+    }
+    const [timestampPart, signaturePart] = signatureHeader.split(",");
+    const [, timestamp] = timestampPart.split("=");
+    const [, signature] = signaturePart.split("=");
+
+    const rawBody = await getRawBody(req);
+
+    const encoder = new TextEncoder();
+    const alg = {
+      name: "HMAC",
+      hash: "SHA-256",
+    } as const;
+    const key = await crypto.subtle.importKey(
+      "raw",
+      encoder.encode(signingSecret),
       alg,
-      key,
-      Buffer.from(signature),
-      Buffer.from(`${timestamp}${rawBody.toString("utf-8")}`)
+      false,
+      ["sign", "verify"]
     );
+
+    try {
+      await crypto.subtle.verify(
+        alg,
+        key,
+        Buffer.from(signature),
+        Buffer.from(`${timestamp}${rawBody.toString("utf-8")}`)
+      );
+    } catch (err) {
+      console.error("Failed to verify signature.", {
+        signature,
+        body: `${timestamp}${rawBody.toString("utf-8")}`,
+      });
+    }
   } catch (err) {
-    console.error("Failed to verify signature.", {
-      signature,
-      body: `${timestamp}${rawBody.toString("utf-8")}`,
+    console.error("Failed to process signature header.", {
+      signatureHeader,
     });
   }
 
