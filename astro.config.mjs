23839772e8eefde47f68ef5dd2b4e1d0a706849e
Richard Simpson <richardsimpson@outlook.com>
2023-03-26 20:09:41 -0500

    make site static for now?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 /* eslint-disable import/order */
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/static";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), partytown({}), tailwind({ config: { applyBaseStyles: false } })],
-  output: "server",
   adapter: vercel(),
   experimental: {
     assets: true
