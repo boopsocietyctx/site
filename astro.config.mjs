6d00e4df8a2a0642bd60deaf508322d612f2dde0
Richard Simpson <richardsimpson@outlook.com>
2023-03-26 20:32:37 -0500

    chore: drop assets and switch back to astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import vercel from "@astrojs/vercel/static";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown({}), tailwind({ config: { applyBaseStyles: false } })],
-  adapter: vercel(),
-  experimental: {
-    assets: true
-  }
+  integrations: [react(), partytown({}), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image()],
+  adapter: vercel()
 });
