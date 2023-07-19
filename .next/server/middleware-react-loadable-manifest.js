911654e122af122786492706646ad6c9967f6dc3
Moises Martinez <moisesm54@gmail.com>
2023-07-18 23:04:03 -0500

    adding cloudflare astro package

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,6 @@
+self.__REACT_LOADABLE_MANIFEST = {
+  "..\\components\\layout.tsx -> ./navbar": {
+    id: "..\\components\\layout.tsx -> ./navbar",
+    files: ["static/chunks/components_navbar_tsx.js"],
+  },
+};
