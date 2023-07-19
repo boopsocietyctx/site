2deb6833888fb878d8172218490ffe472a46f4b7
Moises Martinez <moisesm54@gmail.com>
2023-07-18 23:13:57 -0500

    added back in the tailwind and react imports lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 /* eslint-disable import/order */
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 import cloudflare from '@astrojs/cloudflare';
+import tailwind from "@astrojs/tailwind";
 
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+  integrations: [react(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image()],
+  adapter: cloudflare()
 });
