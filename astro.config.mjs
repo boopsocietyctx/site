975b6451f2046f371af44ab600c465835f4f6128
Moises Martinez <moisesm54@gmail.com>
2023-10-25 14:41:08 -0500

    test: meta redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: {
-    '/officers': '/#officers',
-    '/membership': 'http://eepurl.com/h7kkMf',
-    '/donations': 'https://www.paypal.com/donate/?hosted_button_id=CDKCWWZCGR5NE',
-    '/resources': 'https://drive.google.com/drive/folders/1Ap4VY_FqEy0ybCUKZi8L-jZC0WuLnQl9',
-    '/calendar': 'https://buytickets.at/boopsocietyctx'
-  },
   integrations: [react(), tailwind({
     config: {
       applyBaseStyles: false
