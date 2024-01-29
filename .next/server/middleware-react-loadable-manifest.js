fbb44d52d0ae13607e73f711f60851543522742a
Richard Simpson <richardsimpson@outlook.com>
2024-01-29 12:05:14 -0600

    fix: remove .next leftovers

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,6 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST = {
-  "..\\components\\layout.tsx -> ./navbar": {
-    id: "..\\components\\layout.tsx -> ./navbar",
-    files: ["static/chunks/components_navbar_tsx.js"],
-  },
-};
